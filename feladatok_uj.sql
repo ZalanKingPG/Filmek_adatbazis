41b523d3a752628a78ea1d2ddd9c347014f5d796
Zalan <elnezestmagaon00@gmail.com>
2026-03-26 13:43:28 +0100

    Create feladatok_uj.sql

---
+++ feladatok_uj.sql
@@ -0,0 +1,21 @@
+-- 1. feladat
+SELECT COUNT(*) AS filmek_szama
+FROM filmek;
+-- 2. feladat
+SELECT MAX(hossz) AS leghosszabb_film
+FROM filmek;
+-- 3. feladat
+SELECT SUM(hossz) AS osszes_hossz
+FROM filmek;
+-- 4. feladat
+SELECT AVG(hossz) AS atlag_hossz
+FROM filmek;
+-- 5. feladat
+SELECT MAX(ertekeles) AS legjobb_ertekeles
+FROM filmek;
+-- 6. feladat
+SELECT AVG(ertekeles) AS atlag_ertekeles
+FROM filmek;
+-- 7. feladat
+SELECT COUNT(DISTINCT rendezo) AS rendezok_szama
+FROM filmek;
